1ade1d13650b1faa3da918bcc26cb24d794e620f
James Brundage <james.brundage@start-automating.com>
2021-05-19 02:00:40 -0700

    Adding Module Scaffold

---
+++ PowerNix.psm1
@@ -0,0 +1,9 @@
+﻿foreach ($file in Get-ChildItem -LiteralPath $psScriptRoot -Recurse -Filter *-*.ps1) {
+    . $file.Fullname
+}
+
+
+
+if (-not $PSVersionTable.Platform -or $PSVersionTable.Platform -ne 'Unix') {
+    Write-Warning "PowerNix is not running on Unix.  Most functions will not work."
+}
