404fb6781f86edb8e8f3568000547e9eab902b8a
James Brundage <james.brundage@start-automating.com>
2021-05-19 02:02:58 -0700

    Get-NixUptime: Initial Commit

---
+++ Get-NixUptime.ps1
@@ -0,0 +1,32 @@
+﻿function Get-NixUptime
+{
+    <#
+    .Synopsis
+        Gets uptime for a Unix machine
+    .Description
+        Gets the uptime for a Unix machine, based off of the content of /proc/uptime.
+    .Example
+        Get-NixUptime
+    .Link
+        Get-NixMemory
+    #>
+    [OutputType('PowerNix.Uptime')]
+    param()
+
+    process {
+        #region Check File Exists
+        if (-not (Test-Path '/proc/uptime')) {
+            Write-Error "Missing '/proc/uptime'" -ErrorId File.Missing
+            return
+        }
+        #endregion Check File Exists
+
+        $uptime, $idleTime = @(Get-Content '/proc/uptime' -Raw) -split ' '
+        $uptime = [Timespan]::FromSeconds($uptime)
+        [PSCustomObject]@{
+            PSTypeName = 'PowerNix.Uptime'
+            UpTime = $uptime
+            BootDateTime = ([Datetime]::Now - [Timespan]$uptime)
+        }
+    }
+}
