fe4b1b32a0a3f9aab975a261dad6d1ddaa1e6471
Joshua Corrick <joshua.corrick@live.com>
2021-05-25 03:59:01 +0000

    Add SupportsShouldProcess

--- Mount-Nix.ps1
+++ Mount-Nix.ps1
@@ -4,7 +4,7 @@
     .Synopsis
         Mounts a device
     .Description
-        Mounts a device on a Unix system.  
+        Mounts a device on a Unix system.
         Often requires the user to be running as root.
     .Example
         Mount-Nix -Device //computer/share/ -MountPoint /mnt/computer -FileSystemType cifs -Option rw # Must have the cifs-utils package installed.
@@ -12,6 +12,7 @@
         Get-NixMount
     #>
     [OutputType([Nullable], [string])]
+    [Cmdletbinding(SupportsShouldProcess,ConfirmImpact='High')]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("Test-ForSlowScript", "", Justification="Written for User Clarity")]
     param(
     # The device.
@@ -32,6 +33,7 @@
     $FileSystemType,
 
     # The mount options.  These can either be strings or hashtables.
+    # These are case sensative
     [Parameter(Position=3,ValueFromPipelineByPropertyName)]
     [Alias('Options')]
     [PSObject[]]
@@ -87,22 +89,22 @@
                 "password=$($Credential.GetNetworkCredential().Password)"
             }
         ) -join ','
-        
-        if ($Persistent) { 
-            # If you want a mount to be -Persistent, 
+
+        if ($Persistent) {
+            # If you want a mount to be -Persistent,
             # it has to be written to -FileSystemTablePath (by default /etc/fstab)
-            if ($Credential) { # If we have a credential for a persistent mount, 
+            if ($Credential) { # If we have a credential for a persistent mount,
                 # we don't want to put it in a file _everyone_ can read.
                 # Instead, let's create a hidden file beneath $home, with the last segment of the mountpount.
-                $credFilePath = Join-Path $home ".$(@($MountPoint -split '/')[-1]).creds" 
+                $credFilePath = Join-Path $home ".$(@($MountPoint -split '/')[-1]).creds"
                 $credFileContents = @( # Each line of this file should be outputted as a separate string:
                     "username=$($Credential.GetNetworkCredential().UserName)" # ( the username,
                     "password=$($Credential.GetNetworkCredential().Password)" # the password
-                    # and the domain (if present) 
+                    # and the domain (if present)
                     $(if ($Credential.GetNetworkCredential().Domain) {
                         "domain=$($credential.GetNetworkCredential().Domain)"
-                    }) 
-                ) -join [Environment]::NewLine | # then we join all of the lines with the newlines from the current environemnt. 
+                    })
+                ) -join [Environment]::NewLine | # then we join all of the lines with the newlines from the current environemnt.
                     # (this prevents a file with windows linefeeds from accidentally being created in a Linux environment)
                     Set-Content  $credFilePath
 
@@ -113,17 +115,17 @@
                 if ($chmod) {
                     & $chmod 600 "$credFilePath"
                 }
-                # Add the pointer to the credfile path to the real options            
+                # Add the pointer to the credfile path to the real options
                 $realOptions += ",credentials=$credFilePath"
             }
-            
+
             # Compose the line that we will write to the -FileSystemTablePath.
             # It contains the device, mountpoint, filesystemtype, options, dumpfrequency, and passnumber separated by spaces.
             $line = $Device, $MountPoint, $FileSystemType, $realOptions, $dumpFrequency, $passNumber -join ' '
-            if ($WhatIfPreference) { # If -WhatIf was passed,  
+            if ($WhatIfPreference) { # If -WhatIf was passed,
                  return $line # return the line
             }
-            
+
             foreach ($fileSystemTableLine in Get-Content -Path $FileSystemTablePath) {
                 if ($fileSystemTableLine -eq $line) { # If the line already exists, return
                     return
@@ -132,21 +134,21 @@
                     Write-Warning "Potential Conflict for MountPoint $MountPoint"
                 }
             }
-            
+
             # Add the line to the file system table path
             Add-Content -Path $FileSystemTablePath -Value $line -PassThru:$PassThru
         } else {
             if ($WhatIfPreference) { # If -WhatIf was passed for a dynamic mount
                 return "mount -t $FileSystemType $Device $MountPoint -o $realOptions" # return the command we'd run
-                
+
             }
-            Write-Verbose "mount -t $FileSystemType $Device $MountPoint -o $realOptions" # Write it to -Verbose 
-            mount -t $FileSystemType $Device $MountPoint -o $realOptions  | # Run it 
+            Write-Verbose "mount -t $FileSystemType $Device $MountPoint -o $realOptions" # Write it to -Verbose
+            mount -t $FileSystemType $Device $MountPoint -o $realOptions  | # Run it
                 & { process  {
                     if ($PassThru) { # If -PassThru was passed
                         $_  | Get-NixMount -MountInfoText { $_ } # Turn this back into an object! (see Get-NixMount)
                     }
-                } } 
+                } }
         }
     }
 }
