1c21394c274f22d63c5636530a1f5101731830de
James Brundage <james.brundage@start-automating.com>
2021-06-06 16:25:01 -0700

    Updating workflow (adding triggers)

--- PowerNix.GitHubWorkflow.PSDevOps.ps1
+++ PowerNix.GitHubWorkflow.PSDevOps.ps1
@@ -6,7 +6,7 @@ $workflowPath = Join-Path $PSScriptRoot .github |
     Join-Path -ChildPath workflows |
     Join-Path -ChildPath PowerNix.yml
 
-New-GitHubWorkflow -Name PowerNix -Job PowerShellStaticAnalysis, TestPowerShellOnLinux |
+New-GitHubWorkflow -Name PowerNix -Job PowerShellStaticAnalysis, TestPowerShellOnLinux -On Push, Demand |
     Set-Content $workflowPath -Encoding UTF8
 
 Pop-Location
