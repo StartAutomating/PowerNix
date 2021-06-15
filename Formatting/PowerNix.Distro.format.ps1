c6f471666f91dee4f97d05d85b1e4b04cb930948
James Brundage <james.brundage@start-automating.com>
2021-06-14 17:50:03 -0700

    Adding format definitions for PowerNix.Distro

---
+++ Formatting/PowerNix.Distro.format.ps1
@@ -0,0 +1,3 @@
+﻿Write-FormatView -TypeName PowerNix.Distro -Property PRETTY_NAME, NAME, ID, ID_LIKE, VERSION
+
+Write-FormatView -TypeName PowerNix.Distro -Property HOME_URL, SUPPORT_URL, BUG_REPORT_URL -Name Urls
